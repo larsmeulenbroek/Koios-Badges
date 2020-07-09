f7abfd7804eff4278c435f7c8eed704d589e29b9
Riezzie_3 <richardkerkvliet@hotmail.com>
2020-07-09 15:34:00 +0200

    deployed contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var koiosBadge = artifacts.require("KoiosBadges");
 
 module.exports = function(deployer) {
-  deployer.deploy(koiosBadge, "https://larsmeulenbroek.github.io/Koios-Badges/admin-tool/tokens/");
+  deployer.deploy(koiosBadge);
 };
